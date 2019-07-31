eb080ea06f090e07c243264d3ccef37b23a0ea79
lexmarcos <marcosuel.vieira@hotmail.com>
2019-07-30 22:31:28 -0300

    novo site

---
+++ index.js
@@ -0,0 +1,10 @@
+function opacityPreview(id){
+  let el = document.getElementById(id)
+  el.style.opacity = 1
+  el.style.backgroundPosition = '0px'
+}
+function leavePreview(id){
+  let el = document.getElementById(id)
+  el.style.opacity = 0
+  el.style.backgroundPosition = '306px'
+}
